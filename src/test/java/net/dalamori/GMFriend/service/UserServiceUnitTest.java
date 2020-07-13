194d1ab7a0df4d1dcb32b8b75c714afcc398c346
A. Koelewyn <dalamori@users.noreply.github.com>
2020-07-13 00:59:39 -0700

    chore: test stubs

--- src/test/java/net/dalamori/GMFriend/service/UserServiceUnitTest.java
+++ src/test/java/net/dalamori/GMFriend/service/UserServiceUnitTest.java
@@ -1,9 +1,14 @@
 package net.dalamori.GMFriend.service;
 
+import net.dalamori.GMFriend.repository.UserDao;
+import net.dalamori.GMFriend.services.impl.UserServiceImpl;
 import net.dalamori.GMFriend.testing.UnitTest;
+import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.experimental.categories.Category;
 import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -12,5 +17,33 @@ import org.springframework.test.context.junit4.SpringRunner;
 @Category(UnitTest.class)
 @Ignore
 public class UserServiceUnitTest {
+
+    @Mock
+    private UserDao mockDao;
+
+    private UserServiceImpl impl;
+
+    @Before
+    public void setup() {
+        MockitoAnnotations.initMocks(this.getClass());
+        impl = new UserServiceImpl();
+        impl.setUserDao(mockDao);
+    }
+
+    // create
+
+    // read
+
+    // update
+
+    // delete
+
+    // exists
+
+    // deleteByOwner
+
+    // deleteByGame
+
+    // forGame
 
 }
